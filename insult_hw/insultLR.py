03677b51cc9c331df29f5d6f5eb2b18c4d5a7099
Logan Weir <loganweir@Logans-MacBook-Pro.local>
2013-07-10 13:46:11 -0400

    Insult Homework All Done

---
+++ insult_hw/insultLR.py
@@ -0,0 +1,67 @@
+
+# Usage: python scriptname outputfilename
+
+import numpy as np
+import pandas as pd
+import csv
+import sys
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import auc_score
+
+train = pd.read_csv("/Users/loganweir/DataScience/GADS/my_homework/insult_hw/detrain.csv")
+
+test = pd.read_csv("/Users/loganweir/DataScience/GADS/my_homework/insult_hw/detest.csv")
+
+Comment = train['Comment']
+
+TestComment = test['Comment']
+
+ID1 = test['id']
+
+ID2 = np.array(ID1)
+
+ID = np.reshape(ID2, (2235,1))
+
+Combined = Comment.append(TestComment)
+
+All = np.array(Combined)
+
+vectorizer = CountVectorizer(stop_words='english', ngram_range=(1,3))
+
+Insult =  np.array(train['Insult'])
+
+x_train = vectorizer.fit_transform(All)
+
+model = LogisticRegression()
+
+classifier = model.fit(x_train.toarray()[0:3947,:], Insult)
+
+print(cross_val_score(model, x_train.toarray()[0:3947,:], Insult, cv = 10, score_func=auc_score).mean())
+
+close = classifier.predict_proba(x_train.toarray()[3947:,:])
+
+a = close[:,1:]
+
+finalarray = np.hstack((ID,a))
+
+headers = np.array(['id','Insult'])
+
+predictions = np.vstack((headers, finalarray))
+
+outfile = open(sys.argv[1], 'w')
+
+writer = csv.writer(outfile)
+
+for row in predictions:
+	writer.writerow(row)
+
+outfile.close()
+
+
+
+
+
+
---
+++ regression_hw/InsultNotes.R
@@ -0,0 +1,4 @@
+#combinations given you're or your or ur or you
+#combining logistic and linear functions
+
+train <- read.csv("train.csv", as.is=T)
