c964ec2680d6542bd255b591f978248dabe23a06
Logan Weir <loganweir@Logans-MacBook-Pro.local>
2013-10-01 12:24:26 -0700

    More file updates

---
+++ R ggplot/hw1rscript.R
@@ -0,0 +1,34 @@
+#Data source: http://www.quandl.com/TPC/HIST_FED_TAX_IND_ITR
+#Download as .csv, move to working directory
+
+data <- read.csv ("HIST_FED_TAX_IND_ITR.csv")
+data$Year <- substr(data$Year, 1, 4)
+data$Year <- as.numeric(data$Year)
+
+t1 <- data[,c("Year","Lowest.Quintile")]
+t2 <- data[,c("Year","Middle.Quintile")]
+t3 <- data[,c("Year", "Highest.Quintile")]
+
+t3$Bracket <- "Highest Quintile"
+t2$Bracket <- "Middle Quintile"
+t1$Bracket <- "Lowest Quintile"
+
+colnames(t1) [2] <- "tax"
+colnames(t2) [2] <- "tax"
+colnames(t3) [2] <- "tax"
+
+taxes <- rbind (t1,t2,t3)
+
+tplot <- ggplot(taxes) + aes(Year, tax, color=Bracket)
+tplot <- tplot + geom_path()
+tplot <- tplot + geom_abline(intercept = 0.0, slope =0)
+tplot <- tplot + xlab("Year") + ylab("Individual Income Tax Rate") + ggtitle("Enjoy Your Taxes, Truffle Nibblers")
+tplot <- tplot + annotate("text", x = 1985, y = 12, color = "red", label = "-2.5% Change")
+tplot <- tplot + annotate("text", x = 1985, y = 4, color = "blue", label = "-6.1% Change")
+tplot <- tplot + annotate("text", x = 1985, y = -4, color = "green", label = "-9.3% Change")
+
+tplot
+
+png(filename="hw1logan.png", width=600, height=600)
+tplot
+dev.off()
